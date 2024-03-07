5ac04303a148595fa4655082c4930de714aa253f
Zarathustra123 <157049910+zarathustra2024@users.noreply.github.com>
2024-03-07 17:37:29 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,10 @@
+import random
+
+
 def get_numbers_ticket(min, max, quantity):
-    try
-        
-        min >= 1
-        max <= 1000
-        
-quantity - кількість чисел, які потрібно вибрати (значення між min і max).
+    if min >= 1 and max <= 1000:
+        return random.sample(range(min, max), quantity)
+
+
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
